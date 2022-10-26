41af4c165bf1d2305d19be810789d08da70589c0
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2022-10-26 22:15:06 +0100

    Added more functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+package uk.ac.chester
+
 /**
  * Two inputs: x items + percentage of wanted
  */
@@ -5,31 +7,81 @@
 
 class PercentageCalc {
 
-    fun findValueOf(per: Int, x: Double): Double {
-        return (per / 100) * x
+
+    private var prevValCalc = 0.0
+
+    private fun Double.round(decPlaces: Int) : Double {
+        var finalNumStr = ""
+        var afterDecStr = ""
+        var counter = 0
+        var count = false
+
+        for (num in this.toString()) {
+            if (num == '.') {
+                count = true
+            }
+
+            if (count) {
+                afterDecStr += num
+                counter++
+
+                if (counter > decPlaces) {
+                    break
+                }
+
+            } else {
+                finalNumStr+= num
+            }
+
+
+        }
+
+        if (decPlaces > afterDecStr.length) {
+            val difference = decPlaces - afterDecStr.length
+            for (num in 0..difference) {
+                afterDecStr+= "0"
+            }
+        }
+
+        finalNumStr += afterDecStr
+
+        return finalNumStr.toDouble()
+
     }
 
 
-    fun findValueOf(per: Int, x: Int): Int {
-        return (per / 100) * x
-    }
-
-
-    fun findPercentAndAddOrg(per: Int, x: Int): Int {
-
-        return x + findValueOf(per, x)
+    private fun findValueOf(per: Int, x: Double): Double {
+        prevValCalc = (per / 100) * x
+        return prevValCalc.round(2)
 
     }
 
     fun findPercentAndAddOrg(per: Int, x: Double): Double {
+        prevValCalc = x + findValueOf(per, x)
+        return prevValCalc.round(2)
+    }
 
-        return x + findValueOf(per, x)
 
+    fun findPercentageIncrease(smallerValue: Double, largerValue: Double): Double {
+        val difference = largerValue - smallerValue
+        prevValCalc = (difference / smallerValue) * 100
+        return prevValCalc.round(2)
+
+    }
+
+
+    fun findPercentageDecrease(smallerValue: Double, largerValue: Double): Double {
+        val difference = largerValue - smallerValue
+        prevValCalc = (difference / largerValue) * 100
+        return prevValCalc.round(2)
     }
 }
 
 
 fun main() {
+    val calc = PercentageCalc()
 
+
+    println(calc.findPercentageDecrease(12.0, 22.0))
 
 }
